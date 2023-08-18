bfb1a8f2858cf5807ea212970cbcd548552d5af3
bong94688 <bong94688@gmail.com>
2023-08-18 12:16:29 +0900

    feat-146 : 소켓 통신 초기 세팅

---
+++ src/Pages/seller.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import io from 'socket.io'
+const seller = () => {
+
+  io.
+
+  return (
+    
+    <div>seller</div>
+
+  )
+}
+export const socket = io
+export default seller
