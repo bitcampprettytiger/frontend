a830714a9d35f7ccbea74d7e6795bb3e22c47f71
Eunhee-nim <126763448+Eunhee-nim@users.noreply.github.com>
2023-08-21 13:40:53 +0900

    test : MenuItes 임시

--- src/ShopDetails/SDComponents/MenuItems.jsx
+++ src/ShopDetails/SDComponents/MenuItems.jsx
@@ -1,45 +1,57 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import axios from 'axios';
 
 function MenuItem({ menu, isLast, viewType, fontSize }) {
-  const [menuData, setMenuData] = useState(null);
-
+  const [menuDataList, setMenuDataList] = useState([]);
+  const [data, setData] = useState();
   // useEffect(() => {
   //   if (viewType === 'MenuOrderPage' || viewType === 'MenuSeeMore') {
-  //     // axios.get(`http://27.96.135.75/menu/info/${menu.id}`)
+  //     axios.get(`http://27.96.135.75/menu/info/${menu.id}`)
   //       .then(response => {
-  //     setMenuData(response.data); // 받아온 데이터를 menuData에 저장
-  //   })
+  //         const menuDTOList = response.data.itemlist;
+  //         setMenuDataList(menuDTOList);
+  //       })
   //       .catch(error => {
   //         console.error('메뉴 데이터가 없습니다.', error);
   //       });
+  //     console.log(response)
   //   }
   // }, [menu.vendor.id, viewType]);
+  const test = async () => {
+
+    try {
+      const response = axios.get('http://27.96.135.75/menu/info')
+      setData(response)
+      console.log(response)
+    } catch (e){
+      console.log(e);
+    }
+}
 
   const renderContent = () => {
-    if (viewType === 'MenuOrderPage' && menuData) {
+    if (viewType === 'MenuOrderPage' && menuDataList) {
       return (
         <>
           <div className="menu-image">
-            <img src={menuData.image} alt={menuData.name} />
+            <img src={menuDataList.menuImageList[0]?.url} alt={menuDataList.menuName} />
           </div>
           <div className="menu-info">
-            <h3>{menuData.name}</h3>
-            <p className="menu-description">{menuData.description}</p>
-            <p className="menu-price">가격: {menuData.price}원</p>
+            <h3>{menuDataList.menuName}</h3>
+            <p className="menu-description">{menuDataList.menuContent}</p>
+            <p className="menu-price">가격: {menuDataList.price}원</p>
           </div>
         </>
       );
     } else if (viewType === 'MenuSeeMore') {
       return (
         <>
-          <ListItemText primary={menu.name}
+          <ListItemText primary={menuDataList.menuName}
             primaryTypographyProps={{ sx: { fontSize, fontWeight: 'bold' } }} />
           <ListItemText
-            primary={menu.price}
+            primary={menuDataList.price}
             primaryTypographyProps={{
               sx: {
                 fontSize,
@@ -55,12 +67,14 @@ function MenuItem({ menu, isLast, viewType, fontSize }) {
   };
 
   return (
-    <React.Fragment>
-      <ListItem>
-        {renderContent()}
-      </ListItem>
+    <>
+      {menuDataList.map(menuDataItem => (
+        <ListItem key={menuDataItem.id}>
+          {renderContent(menuDataItem)}
+        </ListItem>
+      ))}
       {!isLast && <Divider />}
-    </React.Fragment>
+    </>
   );
 }
 
