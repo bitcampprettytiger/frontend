1bbe45354e003e6294a12348ef615ada209974ee
mingyu <bidal.mingyu@gmail.com>
2023-08-22 21:49:18 +0900

    Update AppRoute.js

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -12,51 +12,46 @@ import MyTakeout from './Menu/MyPage/MyPageComponents/MyTakeout';
 import GeolocationComponent from './Menu/Home/HomeComponents/GeolocationComponent';
 import MyEdit from './Menu/MyPage/MyPageComponents/MyEdit';
 import ShopMain from './ShopDetails/ShopMain';
-import ReviewForm from './ShopDetails/Review/ReviewForm';;
+import ReviewForm from './ShopDetails/Review/ReviewForm';
 import MyTakeoutDetail from './Menu/MyPage/MyPageComponents/MyTakeoutDetail';
-
-;
+import AppLogin from './Login,Join/login/Login';
+import AppSignup from './Login,Join/join/Register';
 import SellSignUp3 from './Sell/SellSignUp/SSUComponents/SellSignUp3';
 import SellSignUp2 from './Sell/SellSignUp/SSUComponents/SellSignUp2';
 import SellSignUp1 from './Sell/SellSignUp/SSUComponents/SellSignUp1';
 import SellLogin from './Sell/SellJoin/SellLogin';
-export const browserRoutes = [
-  { path: '/', element: <AppLogin /> },
-  { path: '/signup', element: <AppSignup /> },
-  { path: '/home', element: <Home /> },
-  { path: '/trfood', element: <TrFood /> },
-  { path: '/stfood', element: <StFood /> },
-  { path: '/geolocationcomponent', element: <GeolocationComponent /> },
-  { path: '/mypage', element: <Mypage /> },
-  { path: '/search', element: <Search /> },
-  { path: '/waiting', element: <Waiting /> },
-  { path: '/myreview', element: <MyReview /> },
-  { path: '/myfavorite', element: <MyFavorite /> },
-  { path: '/mytakeout', element: <MyTakeout /> },
-  { path: '/waitingDetail', element: <WaitingDetail /> },
-  { path: '/myedit', element: <MyEdit /> },
-  { path: '/shopmain', element: <ShopMain /> },
-  { path: '/sellsign1', element: <SellSignUp1 /> },
-  { path: '/sellsign2', element: <SellSignUp2 /> },
-  { path: '/sellsign3', element: <SellSignUp3 /> },
-  { path: '/selllogin', element: <SellLogin /> },
-];
 
-export const mobileRoutes = [
-  { path: '/', element: <Home /> },
-  { path: '/trfood', element: <TrFood /> },
-  { path: '/stfood', element: <StFood /> },
-  { path: '/geolocationcomponent', element: <GeolocationComponent /> },
-  { path: '/mypage', element: <Mypage /> },
-  { path: '/search', element: <Search /> },
-  { path: '/waiting', element: <Waiting /> },
-  { path: '/myreview', element: <MyReview /> },
-  { path: '/myfavorite', element: <MyFavorite /> },
-  { path: '/mytakeout', element: <MyTakeout /> },
+const commonRoutes = [
+    { path: '/home', element: <Home /> },
+    { path: '/trfood', element: <TrFood /> },
+    { path: '/stfood', element: <StFood /> },
+    { path: '/geolocationcomponent', element: <GeolocationComponent /> },
+    { path: '/mypage', element: <Mypage /> },
+    { path: '/search', element: <Search /> },
+    { path: '/waiting', element: <Waiting /> },
+    { path: '/myreview', element: <MyReview /> },
+    { path: '/myfavorite', element: <MyFavorite /> },
+    { path: '/mytakeout', element: <MyTakeout /> },
+    { path: '/waitingDetail', element: <WaitingDetail /> },
+    { path: '/myedit', element: <MyEdit /> },
+    { path: '/shopHome/vendor:Id', element: <ShopMain /> },
+    // 기타 공통 라우트
+  ];
+  
+  export const browserRoutes = [
+    { path: '/', element: <AppLogin /> },
+    { path: '/signup', element: <AppSignup /> },
+    { path: '/sellsign1', element: <SellSignUp1 /> },
+    { path: '/sellsign2', element: <SellSignUp2 /> },
+    { path: '/sellsign3', element: <SellSignUp3 /> },
+    { path: '/selllogin', element: <SellLogin /> },
+    ...commonRoutes, // 공통 라우트 추가
+  ];
+  
+  export const mobileRoutes = [
+    { path: '/', element: <Home /> },
     { path: '/mytakeoutdetail', element: <MyTakeoutDetail /> },
-  { path: '/waitingDetail', element: <WaitingDetail /> },
-  { path: '/myedit', element: <MyEdit /> },
     { path: '/reviewform', element: <ReviewForm /> },
-  { path: '/shopHome/vendor:Id', element: <ShopMain /> },
-
-];
+    ...commonRoutes, // 공통 라우트 추가
+  ];
+  
