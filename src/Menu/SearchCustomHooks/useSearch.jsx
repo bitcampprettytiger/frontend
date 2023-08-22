5c1c24a9a6b7520ad2afe5a0bd5daa7e129d9c23
yjpotato <ponyojoa7467@gmail.com>
2023-08-22 21:29:59 +0900

    fix-110:오류수정

---
+++ src/Menu/SearchCustomHooks/useSearch.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useSearch = () => {
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+    const [hasSearched, setHasSearched] = useState(false);
+    const [data, setData] = useState([]); // 백엔드에서 가져온 데이터를 저장하기 위한 상태
+
+    useEffect(() => {
+        const API_URL = "http://27.96.135.75/info/vendorId"; // 예시 API URL
+        axios.get(API_URL)
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error("데이터를 불러오는 데 실패했습니다.", error);
+            });
+    }, []);
+
+    const handleSearchChange = (e) => {
+        setSearchInput(e.target.value);
+    }
+
+    const handleDeleteClick = () => {
+        setSearchInput('');
+        setSearchResults([]);
+        setHasSearched(false);
+    };
+
+    const handleHashTagClick = (tag) => {
+        setSearchInput(tag);
+        handleSearchClick(tag);
+    };
+
+    const handleSearchClick = () => {
+        const cleanedSearchInput = searchInput.toLowerCase().replace('#', '');
+        const results = data.filter(item => // `data`를 사용하여 검색
+            (item.name && item.name.toLowerCase().includes(cleanedSearchInput)) ||
+            (item.menu && item.menu.toLowerCase().includes(cleanedSearchInput)) ||
+            (item.content && item.content.toLowerCase().includes(cleanedSearchInput)) ||
+            (item.neighborhood && item.neighborhood.toLowerCase().includes(cleanedSearchInput))
+        );
+        setSearchResults(results);
+        setHasSearched(true);
+    };
+
+    return {
+        searchResults,
+        searchInput,
+        hasSearched,
+        handleSearchChange,
+        handleDeleteClick,
+        handleHashTagClick,
+        handleSearchClick
+    };
+};
+
+export default useSearch;
