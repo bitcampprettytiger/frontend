6e1d00e4ea65d9d9cb7cce0e2af8f0b1ac4bc75f
mingyu <bidal.mingyu@gmail.com>
2023-08-24 12:57:41 +0900

    feat-150 : 회원가입 서버 통신

--- src/Sell/SellSignUp/SSUComponents/SellSignUp3.jsx
+++ src/Sell/SellSignUp/SSUComponents/SellSignUp3.jsx
@@ -19,12 +19,16 @@ import { useNavigate } from 'react-router-dom';
 import SSUHeader from './SSUHeader';
 import SSUAddressModal from './SSUAddressModal';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
+import VendorAPI from './VendorAPI';
 const SellSignUp3 = () => {
   const [address, setAddress] = useState('');
   const [showPostcode, setShowPostcode] = useState(false);
   const [previewImage, setPreviewImage] = useState(null);
   const [activeStep, setActiveStep] = useState(2);
-
+  const [vendorType, setVendorType] = useState('');
+  const [vendorName, setVendorName] = useState('');
+  const [SIGMenu, setSIGMenu] = useState('');
+  const [tel, setTel] = useState('');
   const [businessDays, setBusinessDays] = useState({
     월: false,
     화: false,
@@ -38,7 +42,13 @@ const SellSignUp3 = () => {
     시작: '',
     마감: '',
   });
+  const handleVendorTypeChange = (event) => {
+    setVendorType(event.target.value);
+  };
 
+  const handleVendorNameChange = (event) => {
+    setVendorName(event.target.value);
+  };
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
@@ -53,9 +63,6 @@ const SellSignUp3 = () => {
   };
   const navigate = useNavigate();
 
-  const handleNext = () => {
-    navigate('/selllogin');
-  };
   const handleAddress = (data) => {
     let fullAddress = data.address;
     let extraAddress = '';
@@ -88,7 +95,26 @@ const SellSignUp3 = () => {
       [event.target.name]: event.target.value,
     });
   };
+  const handleNext = async () => {
+    console.log('핸들시작');
+    const result = await VendorAPI(
+      vendorType,
+      vendorName,
+      SIGMenu,
+      address,
+      tel,
+      businessDays,
+      businessHours.시작,
+      businessHours.마감
+    );
 
+    // 여기에 API 호출 후 원하는 작업을 수행 (예: 페이지 이동)
+    if (result === '회원가입 성공!') {
+      navigate('/selllogin');
+    } else {
+      alert('오류가 발생했습니다. 다시 시도해 주세요.');
+    }
+  };
   return (
     <>
       <SSUAddressModal
@@ -105,25 +131,29 @@ const SellSignUp3 = () => {
           상세 정보 작성
         </Typography>
         <form>
+          <Typography variant="body1" sx={{ margin: '2% 0' }}>
+            구분
+          </Typography>
+          {/* 옵션 선택 부분 */}
+          <FormControl variant="outlined" fullWidth>
+            <Select
+              value={vendorType} // 상태에 대한 바인딩
+              onChange={handleVendorTypeChange} // 이벤트 핸들러 바인딩
+            >
+              <MenuItem value={'노점'}>노점</MenuItem>
+              <MenuItem value={'길거리'}>길거리</MenuItem>
+            </Select>
+          </FormControl>
+          {/* 메뉴 선택 부분 */}
           <Grid item xs={12} container alignItems="center">
             <Typography variant="body1" sx={{ margin: '2% 0' }}>
-              구분
+              메뉴 선택
             </Typography>
             <FormControl variant="outlined" fullWidth>
-              <InputLabel>옵션 선택</InputLabel>
-              <Select label="옵션 선택">
-                <MenuItem value={'노점'}>노점</MenuItem>
-                <MenuItem value={'길거리'}>길거리</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} container alignItems="center">
-            <Typography variant="body1" sx={{ margin: '2% 0' }}>
-              대표메뉴
-            </Typography>
-            <FormControl variant="outlined" fullWidth>
-              <InputLabel>메뉴 선택</InputLabel>
-              <Select label="메뉴 선택">
+              <Select
+                value={SIGMenu} // 상태에 대한 바인딩
+                onChange={(e) => setSIGMenu(e.target.value)} // 이벤트 핸들러 바인딩
+              >
                 <MenuItem value={'면'}>면</MenuItem>
                 <MenuItem value={'국물'}>국물</MenuItem>
                 <MenuItem value={'볶음'}>볶음</MenuItem>
@@ -131,111 +161,114 @@ const SellSignUp3 = () => {
               </Select>
             </FormControl>
           </Grid>
-          <Grid container spacing={2}>
-            {[
-              { label: '가게 이름' },
-              { label: '가게 주소', value: address },
-              { label: '가게 번호' },
-            ].map((field) => (
-              <Grid item xs={12} container alignItems="center">
-                <Typography variant="body1" sx={{ margin: '2% 0' }}>
-                  {field.label}
-                </Typography>
-                <TextField
-                  fullWidth
-                  variant="outlined"
-                  placeholder={field.label}
-                  value={field.value || ''}
-                  readOnly={field.label === '가게 주소'}
-                />
-                {field.label === '가게 주소' && (
-                  <Grid item xs={12} container justifyContent="flex-end">
-                    <Button
-                      variant="contained"
-                      onClick={() => setShowPostcode(true)}
-                      sx={{ marginTop: '5%' }}
-                    >
-                      주소 찾기
-                    </Button>
-                  </Grid>
-                )}
-                {field.label === '가게 번호' && (
-                  <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                      <Typography variant="body1">가게 이미지</Typography>
-                    </Grid>
-                    <Grid
-                      item
-                      xs={8}
-                      style={{ display: 'flex', justifyContent: 'flex-end' }}
-                    >
-                      <input
-                        type="file"
-                        onChange={handleImageChange}
-                        style={{ display: 'none' }}
-                        id="file-input"
-                      />
-                      <label htmlFor="file-input">
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          component="span"
-                          sx={{ marginBottom: '5% ' }}
-                        >
-                          파일 선택
-                        </Button>
-                      </label>
-                    </Grid>
-                    <Grid item xs={12} style={{ padding: '0' }}>
-                      <div
-                        style={{
-                          display: 'flex',
-                          justifyContent: 'center', // 이미지를 가운데로 정렬
-                          alignItems: 'center',
-                          height: '200px', // 이미지 박스의 높이를 현재 높이의 절반으로 설정
-                          border: '1px solid #ccc', // 이미지 박스에 테두리 추가
-                          marginLeft: '16px',
-                          textAlign: 'center',
-                        }}
-                      >
-                        <img
-                          src={previewImage}
-                          alt="가게 사진 미리보기"
-                          style={{ width: '50%', maxHeight: '100%' }} // 이미지의 너비를 100%, 최대 높이를 100%로 설정
-                        />
-                      </div>
-                    </Grid>
-                  </Grid>
-                )}
-              </Grid>
-            ))}
-            <Grid item xs={12} container alignItems="center">
-              <Typography variant="body1" sx={{ margin: '2% 0' }}>
-                영업일
-              </Typography>
-              <FormGroup row>
-                {Object.keys(businessDays).map((day) => (
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={businessDays[day]}
-                        onChange={handleBusinessDaysChange}
-                        name={day}
-                      />
-                    }
-                    label={day}
-                  />
-                ))}
-              </FormGroup>
+          {/* 가게 이름 필드 */}
+          <Typography variant="body1" sx={{ margin: '2% 0' }}>
+            가게 이름
+          </Typography>
+          <TextField
+            fullWidth
+            variant="outlined"
+            placeholder="가게 이름"
+            value={vendorName} // 상태에 대한 바인딩
+            onChange={handleVendorNameChange} // 이벤트 핸들러 바인딩
+          />
+          <Grid item xs={12} container alignItems="center">
+            <Typography variant="body1">전화번호</Typography>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="전화번호"
+              value={tel}
+              onChange={(e) => {
+                setTel(e.target.value);
+              }}
+            />
+          </Grid>
+          {/* 가게 주소 필드 */}
+          <Grid item xs={12} container alignItems="center">
+            <Typography variant="body1" sx={{ margin: '2% 0' }}>
+              가게 주소
+            </Typography>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="가게 주소"
+              value={address}
+              readOnly
+            />
+            <Grid item xs={12} container justifyContent="flex-end">
+              <Button
+                variant="contained"
+                onClick={() => setShowPostcode(true)}
+                sx={{ margin: '5% 0' }}
+              >
+                주소 찾기
+              </Button>
             </Grid>
-            <Grid item xs={6} container alignItems="center">
-              <Typography variant="body1" sx={{ margin: '2% 0' }}>
-                영업 시작
-              </Typography>
+          </Grid>
+
+          {/* 가게 이미지 업로드 부분 */}
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <Typography variant="body1">가게 이미지</Typography>
+              <input
+                type="file"
+                onChange={handleImageChange}
+                style={{ display: 'none' }}
+                id="file-input"
+              />
+              <label htmlFor="file-input">
+                <Button variant="contained" color="primary" component="span">
+                  파일 선택
+                </Button>
+              </label>
+            </Grid>
+            <Grid item xs={6} style={{ display: 'flex', alignItems: 'center' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100px',
+                  border: '1px solid #ccc',
+                  marginLeft: '16px',
+                  textAlign: 'center',
+                }}
+              >
+                <img
+                  src={previewImage}
+                  alt="미리보기"
+                  style={{ width: '100%', maxHeight: '100%' }}
+                />
+              </div>
+            </Grid>
+          </Grid>
+          {/* 영업일 체크박스 */}
+          <Grid item xs={12} container alignItems="center">
+            <Typography variant="body1" sx={{ margin: '2% 0' }}>
+              영업일
+            </Typography>
+            <FormGroup row>
+              {Object.keys(businessDays).map((day) => (
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={businessDays[day]}
+                      onChange={handleBusinessDaysChange}
+                      name={day}
+                    />
+                  }
+                  label={day}
+                />
+              ))}
+            </FormGroup>
+          </Grid>
+          {/* 영업 시작 시간 선택 */}
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1">영업 시작</Typography>
               <FormControl variant="outlined" fullWidth>
-                <InputLabel>시작 시간</InputLabel>
                 <Select
-                  label="시작 시간"
                   name="시작"
                   value={businessHours.시작}
                   onChange={handleBusinessHoursChange}
@@ -248,14 +281,10 @@ const SellSignUp3 = () => {
                 </Select>
               </FormControl>
             </Grid>
-            <Grid item xs={6} container alignItems="center">
-              <Typography variant="body1" sx={{ margin: '2% 0' }}>
-                영업 마감
-              </Typography>
+            <Grid item xs={6}>
+              <Typography variant="body1">영업 마감</Typography>
               <FormControl variant="outlined" fullWidth>
-                <InputLabel>마감 시간</InputLabel>
                 <Select
-                  label="마감 시간"
                   name="마감"
                   value={businessHours.마감}
                   onChange={handleBusinessHoursChange}
@@ -269,6 +298,7 @@ const SellSignUp3 = () => {
               </FormControl>
             </Grid>
           </Grid>
+          {/* 완료 버튼 */}
           <Grid
             item
             xs={12}
