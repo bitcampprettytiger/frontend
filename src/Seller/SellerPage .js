2d60b225f1829bc339a6d96921c97f89d4ee5a0d
bong94688 <bong94688@gmail.com>
2023-08-26 12:30:40 +0900

    fix -144 : 소켓연결

--- src/Seller/SellerPage .js
+++ src/Seller/SellerPage .js
@@ -2,40 +2,67 @@ import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 const SellerPage = () => {
-  const [position, setPosition] = useState(0);
-  const [queueSize, setQueueSize] = useState(0);
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [reservationList, setReservationList] = useState([]);
+  const socket = io('http://localhost:8081'); // 판매자 엔드포인트로 연결
 
   useEffect(() => {
-    const socket = io('http://localhost:8081'); // 판매자 엔드포인트로 연결
-
-    socket.addEventListener('open', () => {
-      console.log('Connected to server');
-    });
-
-    socket.addEventListener('message', (event) => {
-      const message = JSON.parse(event.data);
-      updateUI(message.position, message.queueSize, message.phoneNumber);
+    socket.on('message', message => {
+      updateReservationList(message);
     });
 
     return () => {
-      socket.close(); // 컴포넌트 언마운트 시 소켓 연결 해제
+      socket.disconnect(); // 컴포넌트 언마운트 시 소켓 연결 해제
     };
-  }, []);
+  }, [socket]);
 
-  const updateUI = (newPosition, newQueueSize, newPhoneNumber) => {
-    setPosition(newPosition);
-    setQueueSize(newQueueSize);
-    setPhoneNumber(newPhoneNumber);
+  const updateReservationList = newReservation => {
+    setReservationList(prevList => [...prevList, newReservation]);
   };
+
+  const handleAccept = phoneNumber => {
+    socket.emit('accept', phoneNumber);
+  };
+
+
+  const handleEdit = phoneNumber => {
+    // 수정 버튼 클릭 시 처리 로직
+    // 예를 들어, phoneNumber에 해당하는 예약을 찾아 수정하는 코드를 추가해주세요.
+    // reservationList에서 phoneNumber에 해당하는 예약을 찾아 수정할 수 있도록 구현합니다.
+    const updatedReservationList = reservationList.map(reservation => {
+      if (reservation.phoneNumber === phoneNumber) {
+
+        console.log(reservation.phoneNumber)
+        // 수정할 내용을 반영하여 예약 객체를 수정합니다.
+        // 예: reservation.position = newPosition;
+        //     reservation.queueSize = newQueueSize;
+      }
+      return reservation;
+    });
+    setReservationList(updatedReservationList);
+  };
+  const handleDelete = phoneNumber => {
+    // 삭제 버튼 클릭 시 처리 로직
+    // reservationList에서 phoneNumber에 해당하는 예약을 찾아 삭제하는 코드를 추가해주세요.
+    const updatedReservationList = reservationList.filter(reservation => reservation.phoneNumber !== phoneNumber);
+    setReservationList(updatedReservationList);
+  };
+
 
   return (
     <div>
-      <p>Position: {position}</p>
-      <p>Queue Size: {queueSize}</p>
-      <p>Phone Number: {phoneNumber}</p>
+      {reservationList.map((reservation, index) => (
+        <div key={index}>
+          <p>Position: {reservation.position}</p>
+          <p>Queue Size: {reservation.queueSize}</p>
+          <p>Phone Number: {reservation.phoneNumber}</p>
+          <button onClick={() => handleAccept(reservation.phoneNumber)}>허용</button>
+          <button onClick={() => handleEdit(reservation.phoneNumber)}>수정</button>
+          <button onClick={() => handleDelete(reservation.phoneNumber)}>삭제</button>
+        </div>
+      ))}
     </div>
   );
 };
+
 
 export default SellerPage;
---
+++ src/Seller/roll.js
@@ -0,0 +1,81 @@
+import api from './interceptor';
+
+// 점주 줄서기 조회api, method : get, url : /api/que
+export const getQueueList = () => {
+  return api
+    .get(`/api/que/`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// 해당 스토어에 대한 내 줄서기 조회api, method : get, url : /api/que/:store_id
+export const getMyQueue = (storeId) => {
+  return api
+    .get(`/api/que/${storeId}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// 모든 스토어에 대한 내 줄서기 조회api, method : get, url : /api/mypage/que
+export const getMyQueueList = () => {
+  return api
+    .get(`/api/mypage/user/que`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// 줄서기 요청api, method : post, url : /api/que/:store_id
+export const postMyQueue = ({ storeId, editedQueue }) => {
+  return api
+    .post(`/api/que/${storeId}`, editedQueue)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// 줄서기 수정api, method : put, url : /api/que/:que_id
+export const editMyQueue = ({ queId, editedQueue }) => {
+  return api
+    .put(`/api/que/${queId}`, editedQueue)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// 줄서기 삭제api, method : delete, url : /api/que/:que_id
+export const deleteMyQueue = (queId) => {
+  return api
+    .delete(`/api/que/${queId}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// 입장알림api, method : post, url : /api/auth/push
+export const notifyEntrance = (queId) => {
+  return api
+    .post(`/api/auth/reservePush`, { que_id: queId })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// store측 줄서기 거절api, method : post, url : /api/auth/push
+export const rejectEntrance = (queId) => {
+  return api
+    .post(`/api/auth/cancelPush`, { que_id: queId })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
