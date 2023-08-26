dfcc897db48838bed40868ff0fc993b283ed479f
bong94688 <bong94688@gmail.com>
2023-08-26 16:53:03 +0900

    refactory - 144 : 소켓 통신 수정

--- src/Seller/CustomerPage.js
+++ src/Seller/CustomerPage.js
@@ -4,24 +4,23 @@ import { io } from 'socket.io-client';
 const CustomerPage = () => {
   const [position, setPosition] = useState(0);
   const socket = io('http://localhost:8081'); // 서버 주소
-
+  const [vendor,setVendor] = "1";
   useEffect(() => {
     socket.on('message', message => {
       const parsedMessage = JSON.parse(message);
       setPosition(parsedMessage.position);
     });
 
-    return () => {
-      socket.disconnect(); // 컴포넌트 언마운트 시 소켓 연결 해제
-    };
   }, [socket]);
 
   const reserveQueue = () => {
-    // 예약 요청을 서버로 보내는 로직
-    socket.emit('reserve', {});
+    
+    // 서버로 socket.id와 position을 함께 보냄
+    socket.emit('reserve', { socketId: socket.id, position,vendor});
 
     // 여기서는 임의로 위치를 증가시킴
     setPosition(prevPosition => prevPosition + 1);
+    console.log(position);
   };
 
   return (
--- src/Seller/SellerPage .js
+++ src/Seller/SellerPage .js
@@ -6,47 +6,84 @@ const SellerPage = () => {
   const socket = io('http://localhost:8081'); // 판매자 엔드포인트로 연결
 
   useEffect(() => {
+    // const socket = io('http://localhost:8081'); // 판매자 엔드포인트로 연결
+
+    socket.on('customerMatched', data => {
+      // data에는 { socketId, position }가 포함되어 있음
+      console.log('Customer matched:', data);
+      // 이제 Vendor와 매칭된 Customer의 정보를 활용하여 필요한 작업 수행
+    });
+  
     socket.on('message', message => {
-      updateReservationList(message);
+      console.log(message);
+      updateReservationList(JSON.parse(message)); // 서버에서 받은 메시지를 객체로 변환하여 예약 목록 갱신
     });
 
-    return () => {
-      socket.disconnect(); // 컴포넌트 언마운트 시 소켓 연결 해제
-    };
+    // return () => {
+    //   socket.disconnect(); // 컴포넌트 언마운트 시 소켓 연결 해제
+    // };
   }, [socket]);
 
   const updateReservationList = newReservation => {
-    setReservationList(prevList => [...prevList, newReservation]);
+    setReservationList(prevList => [...prevList, newReservation]); // 새로운 예약 정보를 기존 목록에 추가하여 갱신
   };
 
   const handleAccept = phoneNumber => {
-    socket.emit('accept', phoneNumber);
+    socket.emit('accept', phoneNumber); // 예약 허용 요청을 서버로 전송
   };
-
 
   const handleEdit = phoneNumber => {
     // 수정 버튼 클릭 시 처리 로직
-    // 예를 들어, phoneNumber에 해당하는 예약을 찾아 수정하는 코드를 추가해주세요.
-    // reservationList에서 phoneNumber에 해당하는 예약을 찾아 수정할 수 있도록 구현합니다.
     const updatedReservationList = reservationList.map(reservation => {
       if (reservation.phoneNumber === phoneNumber) {
+        // TODO: 서버로 수정 요청 보내기
+        const newData = {
+          newPosition: reservation.position + 1, // 위치 증가
+          newQueueSize: reservation.queueSize - 1, // 대기열 크기 감소
+        };
 
-        console.log(reservation.phoneNumber)
-        // 수정할 내용을 반영하여 예약 객체를 수정합니다.
-        // 예: reservation.position = newPosition;
-        //     reservation.queueSize = newQueueSize;
+        fetch(`http://localhost:8081/edit-reservation/${phoneNumber}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newData),
+        })
+        .then(response => response.text())
+        .then(message => {
+          console.log(message);
+          // 클라이언트에서도 예약 정보 업데이트
+          reservation.position = newData.newPosition;
+          reservation.queueSize = newData.newQueueSize;
+          setReservationList([...reservationList]); // 리스트 갱신
+        })
+        .catch(error => {
+          console.error('Error editing reservation:', error);
+        });
       }
       return reservation;
     });
-    setReservationList(updatedReservationList);
-  };
-  const handleDelete = phoneNumber => {
-    // 삭제 버튼 클릭 시 처리 로직
-    // reservationList에서 phoneNumber에 해당하는 예약을 찾아 삭제하는 코드를 추가해주세요.
-    const updatedReservationList = reservationList.filter(reservation => reservation.phoneNumber !== phoneNumber);
-    setReservationList(updatedReservationList);
+    setReservationList(updatedReservationList); // 수정된 리스트로 갱신
   };
 
+  const handleDelete = phoneNumber => {
+    // 삭제 버튼 클릭 시 처리 로직
+    const updatedReservationList = reservationList.filter(
+      reservation => reservation.phoneNumber !== phoneNumber
+    );
+
+    fetch(`http://localhost:8081/delete-reservation/${phoneNumber}`, {
+      method: 'DELETE',
+    })
+    .then(response => response.text())
+    .then(message => {
+      console.log(message);
+      setReservationList(updatedReservationList); // 리스트 갱신
+    })
+    .catch(error => {
+      console.error('Error deleting reservation:', error);
+    });
+  };
 
   return (
     <div>
@@ -63,6 +100,5 @@ const SellerPage = () => {
     </div>
   );
 };
-
 
 export default SellerPage;
