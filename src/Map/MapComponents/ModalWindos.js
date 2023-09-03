3d2b7c1260ae5c2bf8f5a2ba28a38871df7ec65d
mingyu <bidal.mingyu@gmail.com>
2023-09-03 23:45:02 +0900

    feat-67: 맵 모달 이미지 크기 수정

--- src/Map/MapComponents/ModalWindos.js
+++ src/Map/MapComponents/ModalWindos.js
@@ -6,27 +6,36 @@ import {
   Button,
   Typography,
 } from '@mui/material';
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 const ModalWindows = ({ info, onClose }) => {
-  const navigate = useNavigate(); // useNavigate 훅 사용
+  const navigate = useNavigate();
 
   const goToVendorDetail = () => {
-    // 상세 페이지로 이동
-    console.log(info);
     navigate(`/shophome/${info.vendorid}`);
   };
+
   return (
     <Dialog open={true} onClose={onClose}>
       <DialogContent>
-        {/* 이미지 대신 사용하려면 아래 주석을 해제하세요 */}
-        {/* <img src={info.vendorImage} alt={info.vendorName} /> */}
         <Typography variant="h6">{info.vendorName}</Typography>
-
         <Typography variant="body1">{info.vendorAddress}</Typography>
-        <Typography variant="body1">
-          <img src={info.vendorimg} alt='이미지 있넝'></img>
-        </Typography>
+        <div
+          style={{
+            width: '50%', // 전체 크기의 50%
+            margin: 'auto', // 가운데 정렬
+            textAlign: 'center', // 내부 요소 가운데 정렬
+          }}
+        >
+          <img
+            src={info.vendorimg}
+            alt='이미지 있넝'
+            style={{
+              height:'30vh',
+              width: '100%', // 부모 컨테이너에 꽉 채우기
+            }}
+          />
+        </div>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">
